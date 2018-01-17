da9c2363bc731d14fb1d4ebf97064db8ee3baba9
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2018-01-16 18:55:01 -0800

    working on more design

--- portfolio/portfolio/static/js/app.js
+++ portfolio/portfolio/static/js/app.js
@@ -1,16 +1,16 @@
-handleNav = function() {
-  $('#nav').on('click', 'svg.fa-bars', function() {
-      var $navList = $('#nav_ul');
-      $navList.slideToggle('slow');
-    });
-}
+// handleNav = function() {
+//   $('#nav').on('click', 'svg.fa-bars', function() {
+//       var $navList = $('#nav_ul');
+//       $navList.slideToggle('slow');
+//     });
+// }
 
 
-handleProjects = function() {
-    $('.project_box').hover(function() {
-      var $projectText = $('.project_overlay');
-      $projectText.slideToggle('slow');
-    });
-}
-handleNav()
-handleProjects()
+// handleProjects = function() {
+//     $('.project_box').hover(function() {
+//       var $projectText = $('.project_overlay');
+//       $projectText.slideToggle('slow');
+//     });
+// }
+// handleNav()
+// handleProjects()
