597471cfd3ac84e3ff12138a5fca92eb1139162f
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2018-02-13 14:35:31 -0800

    more style

--- portfolio/portfolio/static/js/app.js
+++ portfolio/portfolio/static/js/app.js
@@ -15,6 +15,20 @@ $(document).ready(function(){
         $(this).toggleClass("active_panel")
         icons.children().toggle()
         $("#" + section + "panel").slideToggle();
+    });   
+});
+
+$(document).ready(function(){
+    $("#_container").css("color", "#181b1e");
+    $("#_container").find(".hidebutton").show()
+    $("section.side_tab").on("click", "li", function() {
+        $(this).siblings().css("color", "white").find(".hidebutton").hide()
+        $(this).css("color", "#181b1e").find(".hidebutton").show()
+        $("section.projects_container").hide()
+        var filter = $(this).attr('id')
+        $(".projects" + filter).show()
+
+       
     });
     
 });
